0ff8c0e5595ebba132e739b1b9e46e0b2f3969ea
rom42pla <romeo.lanzino@gmail.com>
2019-11-23 12:50:30 +0100

    Added the console arguments dataset's filename check

--- scripts/main.py
+++ scripts/main.py
@@ -1,3 +1,5 @@
+import sys
+import os
 import pandas
 
 import utils
@@ -7,8 +9,17 @@ if __name__ == "__main__":
     totalTimer = utils.Timer()
     totalTimer.restartTimer()
 
-    # parses the file into a dataframe
-    dataFilepath = "./data/sample.tsv"
+    consoleArgs = sys.argv[1:]
+    # if no arguments are given, loads the first file in the folder
+    path = "./data"
+    if(len(consoleArgs) == 0):
+        files = [f for f in os.listdir(path) if os.path.isfile(path + "/" + f)]
+        print(files)
+        dataFilepath = path + "/" + files[0]
+    # else loads the specified file
+    else:
+        dataFilepath = consoleArgs[0]
+    # parses the file to a dataframe
     dataframe = parsing.parseFileToDataframe(filepath=dataFilepath, printDataframe=True)
 
     print(f"\nEverything done in {totalTimer.getHumanReadableElapsedTime()}")
